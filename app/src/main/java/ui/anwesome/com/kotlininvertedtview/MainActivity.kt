bc197a57ce80b342ecce1a7851caef09a72592e7
Anwesh43 <anweshthecool0@gmail.com>
2018-04-08 10:34:20 +0530

    adding InvertedTView in activity

--- app/src/main/java/ui/anwesome/com/kotlininvertedtview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlininvertedtview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlininvertedtview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.invertedtview.InvertedTView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        InvertedTView.create(this)
     }
 }
