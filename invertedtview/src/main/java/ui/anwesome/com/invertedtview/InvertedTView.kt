a0c812aefde95bd58d7d4f5b7ca530d6b542ad9b
Anwesh43 <anweshthecool0@gmail.com>
2018-04-08 09:14:52 +0530

    adding InvertedTView class

--- invertedtview/src/main/java/ui/anwesome/com/invertedtview/InvertedTView.kt
+++ invertedtview/src/main/java/ui/anwesome/com/invertedtview/InvertedTView.kt
@@ -3,3 +3,23 @@ package ui.anwesome.com.invertedtview
 /**
  * Created by anweshmishra on 08/04/18.
  */
+
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+
+class InvertedTView (ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
